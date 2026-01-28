5250e2ae9d2ddfcf754c44ce14b817a035b2a424
devairenata1-dotcom <devairenata1@gmail.com>
2026-01-28 22:14:33 +0100

    Fix port NameError

--- app.py
+++ app.py
@@ -1,31 +1,15 @@
 from flask import Flask, render_template, request
-import smtplib
-from email.message import EmailMessage
-from datetime import datetime
+import os
 
 app = Flask(__name__)
-
-def send_email():
-    msg = EmailMessage()
-    msg["Subject"] = "OK gomb megnyomva"
-    msg["From"] = "devairenata1@gmail.com"
-    msg["To"] = "devairenata1@gmail.com"
-
-    msg.set_content(
-        f"Az OK gombot megnyomták.\nIdőpont: {datetime.now()}"
-    )
-
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-        server.login("devairenata1@gmail.com", "bvwu hocd fcsr zxfe")
-        server.send_message(msg)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        send_email()
-        return "<h2>Köszi! Email elküldve 🙂</h2>"
+        print("OK gomb megnyomva")
     return render_template("index.html")
 
+# EZ FONTOS: minden itt van egy blokkban
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
