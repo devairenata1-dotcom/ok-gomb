9a121a344e24eb02140e26076adf939c1911e848
devairenata1-dotcom <devairenata1@gmail.com>
2026-01-29 07:56:02 +0100

    Fix email sending

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from flask import Flask, render_template, request
+import smtplib
+from email.message import EmailMessage
 import os
+from flask import Flask, render_template, request
+
+
+def send_email():
+    msg = EmailMessage()
+    msg["Subject"] = "OK gomb megnyomva 👀"
+    msg["From"] = os.environ.get("EMAIL_FROM")
+    msg["To"] = os.environ.get("EMAIL_TO")
+    msg.set_content("Valaki megnyomta az OK gombot az oldalon.")
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(
+            os.environ.get("EMAIL_FROM"),
+            os.environ.get("EMAIL_PASSWORD")
+        )
+        server.send_message(msg)
+
 
 app = Flask(__name__)
 
+# ⬇️ 3. ROUTE
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -11,7 +30,7 @@ def index():
             print("Email elküldve")
     return render_template("index.html")
 
-# EZ FONTOS: minden itt van egy blokkban
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
